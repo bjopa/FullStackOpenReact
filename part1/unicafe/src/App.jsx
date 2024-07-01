41ba439745f847a0e84d2e416d0f413f85f75165
Björn Wange <bjopa78@gmail.com>
2024-07-01 11:31:40 +0200

    removed some logs

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,13 +6,8 @@ const Header = ({ text }) => {
 
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
-  console.log("g=", good);
-  console.log("n=", neutral);
-  console.log("b=", bad);
-  console.log("t=", total);
   const avg = total > 0 ? ((good - bad) / total).toFixed(1) : 0;
   const positivePercentage = total > 0 ? ((good / total) * 100).toFixed(1) : 0;
-  console.log(avg, positivePercentage);
 
   if (total === 0) return <div>No feedback given</div>;
 
@@ -35,8 +30,7 @@ const Statistics = ({ good, neutral, bad }) => {
 };
 
 const StatisticsLine = (props) => {
-  console.log(props);
-
+  
   if (props.type != null)
     return (
       <tr>
@@ -62,7 +56,7 @@ const Button = (props) => {
 };
 
 const App = () => {
-  // save clicks of each button to its own state
+
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);

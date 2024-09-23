2ee047d9addbd4cdcfdb5ef8163a3701358a4345
Björn Wange <bjopa78@gmail.com>
2024-09-23 09:10:29 +0200

    ex 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -130,9 +130,11 @@ const App = () => {
       </Togglable>
       <br />
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} updateBlogHandler={updateBlog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} updateBlogHandler={updateBlog} />
+        ))}
     </div>
   );
 

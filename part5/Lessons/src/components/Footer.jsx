48eb55ca6ec12c81bb7f9fd3cdb21d607a49f108
Björn Wange <bjopa78@gmail.com>
2024-09-24 07:47:13 +0200

    5-7 cont

--- part5/Lessons/src/components/Footer.jsx
+++ part5/Lessons/src/components/Footer.jsx
@@ -1,15 +1,15 @@
 const Footer = () => {
   const footerStyle = {
-    color: "green",
-    fontStyle: "italic",
+    color: 'green',
+    fontStyle: 'italic',
     fontSizze: 16,
-  };
+  }
   return (
     <div style={footerStyle}>
       <br />
       <em>Note app, Dep of Comp Science, Uni of Helsinki 2024</em>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer

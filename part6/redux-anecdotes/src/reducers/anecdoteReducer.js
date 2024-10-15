d1b9c3a4df7b0e57158ae21ee76d2f778203ed5f
Björn Wange <bjopa78@gmail.com>
2024-10-15 14:57:29 +0200

    ex 6.18

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -8,12 +8,11 @@ const anecdoteSlice = createSlice({
     appendAnecdote(state, action) {
       state.push(action.payload)
     },
-    voteAnecdote(state, action) {
-      const id = action.payload
-      const anecdoteToChange = state.find((a) => a.id === id)
-      if (anecdoteToChange) {
-        anecdoteToChange.votes += 1
-      }
+    updateAnecdote(state, action) {
+      const updatedAnecdote = action.payload
+      return state.map((anecdote) =>
+        anecdote.id !== updatedAnecdote.id ? anecdote : updatedAnecdote
+      )
     },
     setAnecdotes(state, action) {
       return action.payload
@@ -21,7 +20,7 @@ const anecdoteSlice = createSlice({
   },
 })
 
-export const { appendAnecdote, voteAnecdote, setAnecdotes } =
+export const { appendAnecdote, updateAnecdote, setAnecdotes } =
   anecdoteSlice.actions
 
 export const initializeAnecdotes = () => {
@@ -35,6 +34,18 @@ export const createAnecdote = (content) => {
   return async (dispatch) => {
     const newAnecdote = await anecdoteService.createNew(content)
     dispatch(appendAnecdote(newAnecdote))
+  }
+}
+
+export const voteAnecdote = (id) => {
+  return async (dispatch, getState) => {
+    const anecdoteToChange = getState().anecdotes.find((a) => a.id === id)
+    const updatedAnecdote = {
+      ...anecdoteToChange,
+      votes: anecdoteToChange.votes + 1,
+    }
+    const returnedAnecdote = await anecdoteService.update(id, updatedAnecdote)
+    dispatch(updateAnecdote(returnedAnecdote))
   }
 }
 
